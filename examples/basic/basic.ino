52c1ae9de77fe5926333fe174b7f0d72e9d06941
max mc costa <sumotoy@gmail.com>
2013-08-05 20:23:30 +0200

    Update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,126 +1,96 @@
 #include <SPI.h>
-#include <mcp23xxx.h>
 #include <IntMatrixDisp.h>
 /*
+DEMO 'BASIC'
 This library has been written for easily control many old-style alphanumeric led-matrix
  intelligent displays from Avago, Siemens, Osram and so on. Those displays use quite a lot
- of pins so this library allow to control from 1 to 8 displays with 2/3 wire on SPI,WIRE
- on Arduino or Teensy2 or Teensy3 with just 1 MCP23xx17+1 chip, for this reason it's needed
- library mcp23xxx I developed for use as standalone and included in other libraries.
- This is the SPI/Wire version and will be extented for use ShiftRegisters too.
- Actually, here's displays handled:
- DLR3416,DLO3416,DLG3416,DLR2416,DLO2416,DLG2416,
- HDLA2416,HDLS2416,HDLO2416,HDLG2416,
- HPDL1414,DL3416 plus all compatible models (should work but I cannot verify).
- !!! Read documentation about wiring !!!
+ of pins so this library allow to control from 1 to 16 or more displays with 2/3 wire on SPI,WIRE
+ on Arduino/Teensy2/Teensy3 with just 1 MCP23xx17+1 chip.
+ The example uses 2 displays and one MCP23s17.
+ Note that I'm driving MCP23s17 by using it's HAEN specialized option that allow you to use at list
+ 8 MCP23sxx chip compatible HAEN on same wires (include CS pin!)
+ !!! Read documentation inside IntMatrixDisp for wiring and compatible displays !!!
  */
-#define MCP_CS_PIN 10
-#define MCP_ADRS 0x20
-#define DISPLAY_UNITS 1
-/* 
-	Instantiate the library---------------------------
-	cs pin, chip address, how many displays, chip type 
-*/
+#define MCP_CS_PIN 10    // witch CS pin to use, this is the default one called SS
+#define MCP_ADRS 0x20    // HAEN addressing! sounds strange use an hardware addressing in SPI but true
+#define DISPLAY_UNITS 2  // how many units we are using?
+
+// first, create an instance
+// parameters:CS pin, mcp address, display type (see docs)
 IntMatrixDisp mdisp = IntMatrixDisp(MCP_CS_PIN,MCP_ADRS,HDLA2416);
 
 void setup()
 {
-  //Serial.begin(38400);
-  //Serial.println("start");
-  mdisp.init(DISPLAY_UNITS);//necessary! will init SPI also
-  /*
-  This following it's necessary, tell library
-  how to handle the phisical addressing of each display
-  */
-  mdisp.setAddressChip(TTL74HC191);
+  // we first init the display, the SPI and tell library witch addressing method we choosed
+  // PAY ATTENTION! We init the spi here so do not add any spi init after this or before.
+  // parameters:how many displays(numeric),driving method(_DIRECT or _74HC138)
+  mdisp.init(DISPLAY_UNITS,_DIRECT);//necessary! will init SPI also
   delay(100);
   /*
-   not all chip support brightness, if not
-   will not create any problem to uncompatible chip
+   not all chip support brightness, if not it will be ignored
    values:0 to 7. May vary results in some display
+   Parameters:witch display(0=all or display n),value(0..7)
    */
-  mdisp.setBrightness(1,4);
+  mdisp.setBrightness(4);
   delay(100);
   /*
   autoclean will prevent text garbage by clearing
    text from the point selected to the end
    */
   mdisp.setAutoclean(false);
+  //set cursor to a specified char position
   mdisp.setCursor(0);//doesn exat what you mean
-  /*
-  Library does not use arduino text write and have specialized
-   functions:
-   xxx.printString(text,(optional)bool efx);
-   xxx.printNumbers(numeric,(optional)bool efx);
-   xxx.printFloat(float,(optional)byte digits,(optional)bool useSign,(optional)bool efx);
-   digits are decimal numbers (default 2) and has correct rounding
-   useSign put - or + in front number
-   xxx.printDb(uint8_t val,(optional)byte digits,(optional)bool efx,(optional)float maxdb) 
-   this is very useful in audio applications, it convert a digital number (0..255) to
-   it's dB counterpart by using the maxdb parameter that is 6.50. This mean that 255 it's 6.50
-   dB so changing this parameter will modify the convertion algorithm
-   xxx.printTemp(float,(optional)byte digits,(optional)bool mode,(optional)bool efx)
-   another specialized function for write temperature. mode control the sign (cent or far)
-   Beside those functions there's some useful primitive ones:
-   xxx.writeChar(char,digit,(optional)autoposition)
-   This write a single char in the selected digit, the autoposition (normally false) will
-   auto increment the global cursor position so next time you use it it will not cover the
-   existing char.
-   There's a variation xxx.writeChar(char,(optional)autoposition) that not contain digit, in that
-   case use xxx.setCursor(pos);
-   Last function is 
-   xxx.scroll(text,(optional)speed);
-   */
-  mdisp.scroll("Brightness..."); 
-  delay(100);
-  mdisp.printString("wow!",false);
-
-  mdisp.setBrightness(1,7);
-  delay(300);
-
-  mdisp.setBrightness(1,5);
-  delay(100);
-  mdisp.scroll("Numeric Values"); 
-  delay(100);
-  mdisp.printNumber(1234,1);
-  delay(700);
-  mdisp.scroll("Float Values"); 
-  delay(100);
-  mdisp.printFloat((float)8,1,1);
-  delay(700);
-  mdisp.scroll("dB Values"); 
-  delay(100);
-  mdisp.printDb(100,1);
-  delay(700);
-  mdisp.scroll("Any text...");  
-  delay(100);
-  mdisp.printString("CIAO",false);
-  delay(700);
-  mdisp.scroll("Effected...");  
-  delay(100);
-  mdisp.printString("CIAO",true);
-  delay(700);
-  mdisp.printTemp((float)10);
-  delay(300);
+  // scrolling text. On arduino will use a delay but Teensy 3 has the ability to use internal timer for this
+  // parameters: text,speed
+  mdisp.scroll("Universal 2/3 wire led matrix library for arduino and teensy 3",80);
 }
 
 void loop()
 {
-  //  mdisp.scroll("SUMOTOY STUDIOS",150); 
-  //  delay(300);
-  //  randomCharGenerator();
-}
-
-
-void randomCharGenerator(){
-  for (int t=0;t<10;t++){
-    mdisp.clearAll();
-	delay(50);
-    for (int i=0;i<(mdisp.getDigitsPerUnit()*DISPLAY_UNITS);i++){
-      mdisp.writeChar(random(0,127),i);
-      delay(80);
-    }
-  }
+  //clean all display and set cursor to 0
+  mdisp.clearAll();
+  //simply print a string
+  //parameters:text,use efx(0=no,1=yes)
+  mdisp.printString("Numbers",1);
+  delay(100);
+  mdisp.clearAll();
+  //print a number
+  //parameters:number,use efx(0=no,1=yes)
+  mdisp.printNumber(12345678,0);
+  delay(500);
+  mdisp.clearAll();
+  mdisp.printString("Centigrades",1);
+  delay(100);
+  mdisp.clearAll();
+  //print a float
+  //parameters:float number,how many digits,preset (NONE,POL,C,F,Hz,dB,DEG,Hz(automatic Khz,Mhz,etc)),use efx(0=no,1=yes)
+  //note that apart float number the other parameters are optional! In that case we print centigrades
+  mdisp.printFloat((float)22.5,1,C,0);
+  delay(1000);
+  mdisp.setCursor(0);
+  mdisp.printString("Farenheight",1);
+  delay(100);
+  mdisp.clearAll();
+  mdisp.printFloat((float)22.5,1,F,0);
+  delay(1000);
+  mdisp.setCursor(0);
+  mdisp.printString("Decibels",1);
+  delay(100);
+  mdisp.clearAll();
+  mdisp.printFloat((float)18.5,1,dB,0);
+  delay(1000);
+  mdisp.setCursor(0);
+  mdisp.printString("Degrees",1);
+  delay(100);
+  mdisp.clearAll();
+  mdisp.printFloat((float)160.5,1,DEG,0);
+  delay(1000);
+  mdisp.setCursor(0);
+  mdisp.printString("Frequency (plus efx)",1);
+  delay(100);
+  mdisp.clearAll();
+  mdisp.printFloat((float)16550.5,1,Hz,1);
+  delay(1000);
 }
 
 
