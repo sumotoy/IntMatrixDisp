61e836ed605e92044e4b3c2cb04d7416565167e7
max mc costa <sumotoy@gmail.com>
2013-08-05 20:23:59 +0200

    Update basic2.ino

--- examples/basic2/basic2.ino
+++ examples/basic2/basic2.ino
@@ -1,50 +1,38 @@
-/*
-                               _                
- ___  _   _  _ __ ___    ___  | |_  ___   _   _ 
- / __|| | | || '_ ` _ \  / _ \ | __|/ _ \ | | | |
- \__ \| |_| || | | | | || (_) || |_| (_) || |_| |
- |___/ \__,_||_| |_| |_| \___/  \__|\___/  \__, |
- |___/ 
- 
- --------------------------------------------------------------------------------
- A fast C-Library for several 'Intelligent Led Matrix Displays'
- that works with just 2/3 wires though 2 shift registers or 1 GPIO extender chip,
- for most Arduinos, Arduino 2 and Teensy (3 included).
- version 1 (12 March 2013)
- coded by Max MC Costa for s.u.m.o.t.o.y - sumotoy@gmail.com
- --------------------------------------------------------------------------------
- Demo 2 - demonstrate the ability of the library to easy show any type
- of value or alphanumeric in 1 to 8 display units easily (each unit = 4 char)
- This demo use a MCP23S17 GPIO extender so it works in SPI mode.
- */
-
 #include <SPI.h>
-#include <mcp23xxx.h>
 #include <IntMatrixDisp.h>
 
 #define MCP_CS_PIN 10
 #define MCP_ADRS 0x20
-#define DISPLAY_UNITS 3
+#define DISPLAY_UNITS 2
 
 IntMatrixDisp mdisp = IntMatrixDisp(MCP_CS_PIN,MCP_ADRS,HDLA2416);
 
 void setup()
 {
-  //Serial.begin(38400);
-  //Serial.println("start");
-  mdisp.init(DISPLAY_UNITS,DIRECT);//necessary! will init SPI also
-
-  mdisp.setBrightness(0,3);
+  // we first init the display, the SPI and tell library witch addressing method we choosed
+  // PAY ATTENTION! We init the spi here so do not add any spi init after this or before.
+  // parameters:how many displays(numeric),driving method(_DIRECT or _74HC138)
+  mdisp.init(DISPLAY_UNITS,_DIRECT);//necessary! will init SPI also
   delay(100);
+
+  //mdisp.setBrightness(3);
+  //delay(100);
   mdisp.setAutoclean(true);
 
   mdisp.scroll("Intelligent Matrix-LED display library v1.0",80);
-  for (byte i=1;i<4;i++){
-    mdisp.printString("Intensity...",1);
-    mdisp.setBrightness(4-i,0);
-    delay(500);
+  for (int i=0;i<8;i++){
+    mdisp.setBrightness(7-i);
+    mdisp.setCursor(0);
+    mdisp.printString("Int: ");
+    mdisp.printNumber(7-i,0);
+    if (i == 7 || i == 0) {
+      delay(700);
+    } else {
+      delay(300);
+    }
+    
   }
-   mdisp.setBrightness(0,0);
+  mdisp.setBrightness(3);
 }
 
 
